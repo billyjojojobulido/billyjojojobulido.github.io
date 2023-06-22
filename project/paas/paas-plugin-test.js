dfa811d3aa807c560136b95388fd47a119cdd80e
Baocheng Wang <bwan3675@uni.sydney.edu.au>
2023-06-23 01:12:52 +1000

    <new> migrating Dr. Nir's web page template

---
+++ project/paas/paas-plugin-test.js
@@ -0,0 +1,83 @@
+---
+title: Planimation
+summary: Planimation is a framework to visualise sequential solutions of planning problems specified in PDDL
+tags:
+- AI Planning
+- Explainable AI (XAI)
+date: "2019-04-27T00:00:00Z"
+
+links:
+- name: Documentation
+  url: https://planimation.github.io/documentation/
+  # icon_pack: fas
+  # icon: books
+url_code: "https://github.com/AI-Planning/planning-as-a-service"
+url_pdf: "https://arxiv.org/abs/2008.04600"
+url_video: "https://www.youtube.com/watch?v=Cj2rWdt1YQU&feature=youtu.be"
+
+
+# Optional external URL for project (replaces project detail page).
+external_link: ""
+
+image:
+  caption: Planimation
+  focal_point: Smart
+  preview_only: true
+
+---
+
+
+Planimation is a modular and extensible open source framework to visualise sequential solutions of planning problems specified in PDDL. We introduce a preliminary declarative PDDL-like animation profile specification, expressive enough to synthesise animations of arbitrary initial states and goals of a benchmark with just a single profile.
+
+## Overview 
+
+Planimation was developed as a final year project by students at the Univeristy of Melbourne. Here's a promotional video explaining the motivation behind planimation.
+
+{{< youtube FEm5ZQM-b3Y >}}
+
+Planimation is an open-source program which visualises solutions to Planning Problems. It is an executable Unity architecture in a browser.
+
+Watch the video below for a 2 minutes introduction about Planimation.
+
+{{< youtube Cj2rWdt1YQU >}}
+
+## Awards
+
+### ICAPS 2019 Best System Demonstration
+Recipient of the [ICAPS 2019 Best system Demo Award](https://icaps19.icaps-conference.org/awards).
+
+{{< tweet 1151380531021434880 >}}
+
+### Melbourne School of Engineering - Endeavour 2018
+
+[Endeavour Exhibition](https://endeavour.unimelb.edu.au/past/2018-endeavour-exhibition) discovers final-year industry, design and research projects developed by our engineering and IT masters students.
+
+
+
+![image](https://endeavour.unimelb.edu.au/__data/assets/image/0007/2914621/endeavour-2018-11.jpg)
+![image](https://endeavour.unimelb.edu.au/__data/assets/image/0005/2914691/endeavour-2018-36.jpg)
+
+## Citing Planimation
+
+[Technical Report - ICAPS 2019 System Demonstration](https://arxiv.org/abs/2008.04600)
+
+Please cite Planimation using
+``` latex
+@article{chen2020planimation,
+  author       = {Chen, Gan and
+                  Ding, Yi and
+                  Edwards, Hugo and
+                  Chau, Chong Hin and
+                  Hou, Sai and
+                  Johnson, Grace and
+                  Sharukh Syed, Mohammed and
+                  Tang, Haoyuan and
+                  Wu, Yue and
+                  Yan, Ye and
+                  Tidhar Gil and
+                  Lipovetzky Nir},
+  title        = {Planimation},
+  journal      = {arXiv preprint arXiv:2008.04600},
+  year         = {2020}
+}
+```
